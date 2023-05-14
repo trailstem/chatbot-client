b4c787cd73a2b82a335d3f9ab6b9f2f364cda690
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-05-14 14:52:22 +0900

    スタイル修正、リクエストしてからレスポンスが帰ってくるまで送信ボタンを非活性に実装

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useCallback, useState } from "react";
 
-import TextBox from "./components/TextBox";
+import ResizeTextarea from "./components/ResizeTextarea";
 import Button from "./components/Button";
-
+import TextArea from "./components/TextArea";
+const API_URL = process.env.REACT_APP_GO_API_URL;
 function App() {
   //ユーザの入力状態管理
   const [inputText, setInputText] = useState("");
@@ -12,17 +13,21 @@ function App() {
   // 過去10件のチャット履歴状態管理
   const [historyChatList, setHistoryChatList] = useState([]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [value, setValue] = useState("");
+
   // テキストボックスの入力値が変わった時に呼ばれる関数
   const handleInputChange = useCallback((e) => {
     setInputText(e.target.value);
   }, []);
 
   // 環境変数を参照
-  const API_URL = process.env.REACT_APP_GO_API_URL;
 
   //実際のリクエスト処理
   const handleSubmit = useCallback(
     async (e) => {
+      setIsLoading(true);
       e.preventDefault();
       const formData = {
         user_input: inputText,
@@ -48,33 +53,37 @@ function App() {
         setInputText("");
         setCurrentChat([...currentChat, data.response]);
       }
+      setIsLoading(false);
     },
-    [inputText, currentChat, API_URL]
+    [inputText, currentChat]
   );
 
-  const handleClick = useCallback(async (e) => {
-    e.preventDefault();
-    // リクエストオプションを設定
-    const requestOptions = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    console.log(requestOptions.body);
-    //ログインエンドポイントにリクエスト
-    const response = await fetch(`${API_URL}/history/list`, requestOptions);
-    if (!response.ok) {
-      // レスポンスがエラーだった場合
-      const data = await response.json();
-      alert(data.error);
-    } else {
-      // レスポンスが正常だった場合
-      const data = await response.json();
-      console.log(data);
-      setHistoryChatList(data.history_list);
-    }
-  }, []);
+  const handleClick = useCallback(
+    async (e) => {
+      e.preventDefault();
+      // リクエストオプションを設定
+      const requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      console.log(requestOptions.body);
+      //ログインエンドポイントにリクエスト
+      const response = await fetch(`${API_URL}/history/list`, requestOptions);
+      if (!response.ok) {
+        // レスポンスがエラーだった場合
+        const data = await response.json();
+        alert(data.error);
+      } else {
+        // レスポンスが正常だった場合
+        const data = await response.json();
+        console.log(data);
+        setHistoryChatList(data.history_list);
+      }
+    },
+    [currentChat]
+  );
 
   const toDate = (date) => {
     //en-USを日本時刻に設定
@@ -92,31 +101,50 @@ function App() {
     <>
       {/* 独自作成したTextBoxコンポーネントを使用 */}
       <form onSubmit={handleSubmit}>
-        <div className="flex justify-center items-center">
-          <TextBox
-            //画面左側に広がるようにclassName設定
-            className="w-1/2 h-10 border-2 border-gray-200 rounded"
-            placeholder="質問したい内容を入力してください"
-            type="text"
-            value={inputText}
-            onChange={handleInputChange}
-          />
-          <Button
-            name="送信"
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          />
+        <div className="mt-10">
+          <div className="flex justify-center items-center">
+            <ResizeTextarea
+              value={inputText}
+              className="w-1/2 border-2 border-gray-200 resize-none rounded-md"
+              placeholder="質問したい内容を入力してください"
+              onChange={handleInputChange}
+            />
+            <div className="pl-3">
+              <Button
+                name="送信"
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                disabled={isLoading}
+              />
+            </div>
+          </div>
         </div>
-        <div className="flex justify-center items-center">
-          <div className="w-1/2 h-10">
+
+        <div className="flex justify-center">
+          <div className="w-2/3 h-10">
             <ul>
               {currentChat.map((data, index) => (
-                <li key={index} className="chat-item">
-                  <div className="user-input">
-                    {toDate(data.response_timestamp)} You ＞{data.user_input}
+                <li key={index} className="">
+                  <div className="flex items-start">
+                    <div>{toDate(data.response_timestamp)} You ＞</div>
+                    <div className="flex-grow">
+                      <TextArea
+                        value={data.user_input}
+                        className="w-full resize-none overflow-hidden bg-white border-none rounded-md"
+                        isDisabled={true}
+                      />
+                    </div>
                   </div>
-                  <div className="bot-response">
-                    {toDate(data.response_timestamp)} Bot ＞{data.bot_response}
+                  <div className="flex items-start">
+                    <div>{toDate(data.response_timestamp)} Bot ＞</div>
+                    <div className="flex-grow">
+                      <ResizeTextarea
+                        value={data.bot_response}
+                        className="w-full resize-none overflow-hidden bg-white border-none rounded-md"
+                        isDisabled={true}
+                        onChange={handleInputChange}
+                      />
+                    </div>
                   </div>
                 </li>
               ))}
