80873874c2002745a4a00ed3bad43badda09a6b1
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-05-15 00:16:49 +0900

    コメントアウト修正

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-// expressモジュールを読み込む
-const express = require("express");
-const cors = require("cors");
-// pathモジュールを読み込む
-const path = require("path");
-// portは、環境変数のポート番号またはデフォルトの3000を使用する
-const port = process.env.PORT || 3000;
-// expressアプリケーションのインスタンスを作成する
-const app = express();
-// '/build'ディレクトリの静的ファイルを通常通りに提供する
-app.use(express.static(__dirname + "/build"));
-// その他のすべてのルートはindex.htmlを返すようにする
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "build", "index.html"));
-});
-// ポートをリッスンし、サーバを起動する
-app.listen(port);
-console.log("Server started on port " + port);
+// // expressモジュールを読み込む
+// const express = require("express");
+// // pathモジュールを読み込む
+// const path = require("path");
+// // portは、環境変数のポート番号またはデフォルトの3000を使用する
+// const port = process.env.PORT || 3000;
+// // expressアプリケーションのインスタンスを作成する
+// const app = express();
+// // '/build'ディレクトリの静的ファイルを通常通りに提供する
+// app.use(express.static(__dirname + "/build"));
+// // その他のすべてのルートはindex.htmlを返すようにする
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "build", "index.html"));
+// });
+// // ポートをリッスンし、サーバを起動する
+// app.listen(port);
+// console.log("Server started on port " + port);
